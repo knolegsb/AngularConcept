6c4e8107687a328d2df40e39061ce7f087b3f990
knolegsb <seon2you@hotmail.com>
2016-01-21 13:25:25 -0800

    book browser

---
+++ js/bookBrowserApp.js
@@ -0,0 +1,18 @@
+﻿(function () {
+    'use strict';
+
+    angular.module('bookBrowserApp', [])
+        .service('bookService', function ($http) {
+            return {
+                getImages: function () {
+                    return $http.get("/data/books.json", { responseType: 'json' });
+                }
+            }
+        })
+        .controller('bookListCtrl', function ($scope, bookService) {
+            $scope.txtTitle = 'Book List';
+            bookService.getImages().success(function (result) {
+                $scope.bookImages = result;
+            });           
+        });
+})();
